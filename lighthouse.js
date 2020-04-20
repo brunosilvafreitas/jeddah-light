8d3348a237b5a9c2c9e89830cb2d37789980cc53
Bruno Freitas <bruno.freitas@luizalabs.com>
2020-04-20 08:50:48 -0300

    chore: write files

--- lighthouse.js
+++ lighthouse.js
@@ -1,7 +1,12 @@
+const { promisify } = require('util')
+const fs = require('fs')
+const path = require('path')
 const { URL } = require('url')
 const puppeteer = require('puppeteer')
 const lighthouse = require('lighthouse')
 
+const writeFile = promisify(fs.writeFile)
+const outputDir = '.output'
 const config = {
   output: ['html', 'json'],
   emulatedFormFactor: 'desktop'
@@ -10,9 +15,12 @@ const config = {
 const launchLighthouse = async (url) => {
   const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
   config.port = new URL(browser.wsEndpoint()).port
-  const lhr = await lighthouse(url, config)
+  const results = await lighthouse(url, config)
   await browser.close()
-  return lhr
+  await config.output.forEach(async (extention, index) => {
+    await writeFile(path.join(__dirname, outputDir, `lighthouse.${extention}`), results.report[index], 'utf8')
+  })
+  return results.lhr
 }
 
 // use results.lhr for the JS-consumable output
@@ -21,5 +29,5 @@ const launchLighthouse = async (url) => {
 // use results.artifacts for the trace/screenshots/other specific case you need (rarer)
 
 launchLighthouse('https://magazineluiza.com.br/')
-  .then(res => console.log(res.report[0]))
+  .then(res => console.log(res))
   .catch(e => console.error(e))
