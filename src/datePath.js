d7cf8e6820f218609f6204a62fbf2ad4786fb6be
Bruno Freitas <bruno.freitas@luizalabs.com>
2020-04-25 14:12:21 -0300

    chore: improve writefile

--- src/datePath.js
+++ src/datePath.js
@@ -1,4 +1,3 @@
-const fs = require('fs')
 const path = require('path')
 const moment = require('moment-timezone')
 const { timezone } = require('../config')
@@ -7,9 +6,7 @@ const config = require('../config')
 const getDatePath = ({ date = new Date().toUTCString(), file = '' } = {}) => {
   const mDate = timezone ? moment(date).tz(timezone) : moment(date)
   const datePath = mDate.format('/YYYY/MM/DD/HH/mm/')
-  const dirPath = path.join(__dirname, config.outputDir, datePath)
-  fs.mkdirSync(dirPath, { recursive: true })
-  return path.join(dirPath, file)
+  return path.join(__dirname, config.outputDir, datePath, file)
 }
 
 module.exports = getDatePath
--- src/writeFile.js
+++ src/writeFile.js
@@ -1,5 +1,12 @@
 const fs = require('fs')
 const { promisify } = require('util')
 const writeFile = promisify(fs.writeFile)
+const mkdir = promisify(fs.mkdir)
 
-module.exports = writeFile
+const write = async (file, data, ...args) => {
+  const dir = file.replace(/\/+\w*.\w*$/, '')
+  await mkdir(dir, { recursive: true })
+  return writeFile(file, data, ...args)
+}
+
+module.exports = write
